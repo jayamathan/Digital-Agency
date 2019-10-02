1850e20d4e40ba11c0c01ce55ab85ae3256ca2ec
jayamathan <jayamathanskj03@gmail.com>
2019-10-02 08:21:08 +0530

    Add site content and Make fully responsive & create a project page

--- dist/Landing.a7136427.js
+++ dist/Landing.a7136427.js
@@ -21203,7 +21203,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58795" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56568" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
---
+++ src/index.js
@@ -0,0 +1,10 @@
+
+
+import GlobalAnimations from './scripts/Global/Animations/GlobalAnimations';
+ 
+import MobileNav from './scripts/mobileNav/mobileNav';
+
+
+ 
+ 
+ 
